79a3b47188175eeb90d1eb7bebf218df28a636f1
s30696 <s30696@pjwstk.edu.pl>
2026-03-15 14:55:39 +0100

    x

--- zadanie15marzec/Program.cs
+++ zadanie15marzec/Program.cs
@@ -1,6 +1,93 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("s30084pjatk github");
 
 
+public class Device
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    //public bool isAvailableToRent { get; set; }
+    // public bool state (availableToRent, broken, notAvailable)
+    public string StanZuzycia { get; set; }
+    
+    // kontruktor
+    public void showAllItemsWithTheirState() {}
+    public void showAllItemsAvailableToRent() {}
+    
+    public 
+}
+
+public class Laptop
+{
+    // kontruktor
+}
+
+public class Camera
+{
+    // kontruktor
+}
+
+public class Monitor
+{
+    // kontruktor
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Type {get; set;}
+    // Add new user (konstruktor metoda)
+    
+}
+
+public class Student
+{
+    
+}
+
+public class Employee
+{
+    
+}
+
+public class Renting
+{
+    public long StartingDate { set; get; }
+    public long PlannedEndDate { set; get; }
+    public long RealEndDate { set; get; }
+    public long Fee { set; get; }
+    public long AdditionalCosts { set; get; }
+    public long UserId { set; get; }
+    public long DeviceId { set; get; }
+    public bool WasReturnOnTime { set; get; }
+
+
+    // Konstruktor, ktory pozwala wypozyczyc
+    public void returnDevice(int DeviceId)
+    {
+        // check if return date extend planned end date. If it is true then calcualte additional costs
+    }
+
+    //public static showAllRentedDeviceForCertainUser(//userId) { }
+
+    //public static showAllRentingObjectsWithNotReturnBeforeDeadline
+
+    //public generateRaportAboutAllRentings() {
+    // show current rentedObjects
+    // show how many Objects where rentened to this date
+    //}
+
+}
+
+public class Serwis
+{
+    
+}
+
+void main()
+{
+    // Add some items
+}
