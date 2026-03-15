ebcc9f0e375ee1743d4eab78d35b3b69b11cf9d7
s30696 <s30696@pjwstk.edu.pl>
2026-03-15 14:28:40 +0100

    check

--- zadanie15marzec/Program.cs
+++ zadanie15marzec/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("s30084pjatk github");
 
 
